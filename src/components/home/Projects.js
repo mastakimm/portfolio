1c93073470e1fd140d526fd1be408f7847655193
mastakimm <antoinefawer39@hotmail.com>
2024-06-21 10:30:27 +0200

    [FIX] only 3 main projects

--- src/components/home/Projects.js
+++ src/components/home/Projects.js
@@ -5,8 +5,8 @@ import CaseStudyButton from "../../assets/buttons/CaseStudyButton";
 
 
 const project1 = {
-    title: "Y",
-    description: "This page contains the case study of Y ProjectDetails which includes the ProjectDetails Overview, Tools Used and Live Links to the official product.",
+    title: "YZ",
+    description: "This page contains the case study of YZ ProjectDetails which includes the ProjectDetails Overview, Tools Used and Live Links to the official product.",
     backgroundImage: "../../assets/sections/home_section_background.jpg",
     image: "../../assets/sections/project_section_project_1.png",
     overview: "This is a brief description of the project. This is a brief description of the project.",
@@ -60,10 +60,10 @@ const Projects = React.forwardRef((props, ref) => {
                 <div className="md:w-9/12 text-gray-800 p-4 xxl:px-16 xl:pr-12 py-10">
                     <h3
                         className="text-2xl text-center sm:text-start md:text-start lg:text-start xl:text-start 2xl:text-start 3xl:text-start sm:text-3xl md:text-3xl 3xl:text-4xl font-bold mb-4">
-                        Y
+                        YZ
                     </h3>
                     <p className="text-xl sm:text-xl md:text-2xl lg:text-xl xl:text-lg 3xl:text-3xl py-4">
-                        Y, a social network resembling Twitter, lets you share moments and engage with your community.
+                        YZ, a social network resembling Twitter, lets you share moments and engage with your community.
                         Stay informed and connected with Y's user-friendly platform. Experience social networking at its best.
                     </p>
                     <div
@@ -115,29 +115,6 @@ const Projects = React.forwardRef((props, ref) => {
                         <CaseStudyButton buttonText={"Case Study"} project={project3}/>
                     </div>
                 </div>
-            </div>
-            <div
-                className="p-4 pt-4 pb-20 md:p-12 lg:p-20 xl:p-24 3xl:p-24 rounded-lg flex flex-col-reverse xl:flex-row items-center space-y-4 md:space-y-4 xl:space-x-48 3xl:space-x-64">
-                <div className="md:w-9/12 text-gray-800 p-4 xl:pr-12 py-10">
-                    <h3
-                        className="text-2xl text-center sm:text-start md:text-start lg:text-start xl:text-start 2xl:text-start 3xl:text-start sm:text-3xl md:text-3xl 3xl:text-4xl font-bold mb-4">
-                        ProjectDetails Title
-                    </h3>
-                    <p className="text-xl sm:text-2xl md:text-2xl lg:text-xl xl:text-lg 3xl:text-3xl py-4">
-                        This is a brief description of the project. This is a brief description of the project. This is
-                        a brief description of the project. This is a brief description of the project. This is a brief
-                        description of the project.
-                    </p>
-                    <div
-                        className="flex justify-center items-center sm:block md:block lg:block xl:block 2xl:block 3xl:block">
-                        <CaseStudyButton buttonText={"Case Study"}/>
-                    </div>
-                </div>
-                <img
-                    src={ProjectImage1}
-                    alt="ProjectDetails 1"
-                    className="w-full md:w-3/4 lg:w-2/3 xl:w-5/12 rounded-lg"
-                />
             </div>
             <h4
                 className="text-center text-3xl font-bold italic border-b-2 border-black pt-20"
